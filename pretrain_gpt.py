4bd5cf9db9e44bbd416f3ddcb22420c57b7e6315
Daniel Tamayo Mela <danitamayo.adhoc@gmail.com>
2026-09-10 10:51:18 +0200

    Mask assistant delimiter token losses

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -316,6 +316,7 @@ def core_gpt_dataset_config_from_args(args):
         "reset_position_ids": args.reset_position_ids,
         "reset_attention_mask": args.reset_attention_mask,
         "eod_mask_loss": args.eod_mask_loss,
+        "loss_mask_token_ids": tuple(args.mask_loss_token_ids) if args.mask_loss_token_ids else None,
         "create_attention_mask": args.create_attention_mask_in_dataloader,
         "object_storage_cache_path": args.object_storage_cache_path,
         "mid_level_dataset_surplus": args.mid_level_dataset_surplus,
